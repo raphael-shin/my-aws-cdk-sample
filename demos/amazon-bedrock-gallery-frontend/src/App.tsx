deb9571b5277fbd3686b981acfbef3ac8c6fe4d9
Kihoon Kwon <hoon9804@gmail.com>
2024-07-18 13:55:16 +0900

    remove unnecessary codes

--- demos/amazon-bedrock-gallery-frontend/src/App.tsx
+++ demos/amazon-bedrock-gallery-frontend/src/App.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import { Amplify } from 'aws-amplify';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PhotoStart from './PhotoStart';
 import PhotoTake from './PhotoTake';
 import ImageDisplay from './ImageDisplay';
-import { Authenticator } from '@aws-amplify/ui-react';
 import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import '@aws-amplify/ui-react/styles.css';
-
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: 'us-west-2_j4di9vdEv',
-      userPoolClientId: '53krpd79r9qdnigc6754j00do9',
-    }
-  }
-});
 
 const queryClient = new QueryClient({
   defaultOptions: {
